48315c91a353956717abb94156fd10aa9e693ad9
Sarah <sarahsilva@live.com>
2016-12-09 21:32:37 -0600

    first commit

---
+++ index.ios.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {Provider} from 'react-redux';
+import {
+  AppRegistry,
+} from 'react-native';
+
+import Main from './app/components/Main';
+import {configureStore} from './app/store';
+
+class reduxTodoList extends Component {
+  render() {
+    return (
+      <Provider store = {configureStore()} >
+        <Main/ >
+      </Provider>
+    );
+  }
+}
+
+AppRegistry.registerComponent('reduxTodoList', () => reduxTodoList);
